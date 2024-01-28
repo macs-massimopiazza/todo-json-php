58ee4035915dcbae9342e75d03b809c69a3d4f89
macs-massimo-piazza <me@massimopiazza.com>
2024-01-28 22:37:27 +0100

    b

--- back/index.php
+++ back/index.php
@@ -1,2 +1,7 @@
 <?php
-echo "ciaoooo";
+$razze_cani = ["corgi", "carlino", "maltipoo", "pitpull", "golden retriver", "gatto"];
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173");
+
+echo json_encode($razze_cani);
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      razze: [],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8888/todo-json-php/back/").then((res) => {
+      this.razze = res.data;
+    });
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Le razze di caniii :D</h1>
+  <ul>
+    <li v-for="razza in razze">{{ razza }}</li>
+  </ul>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
